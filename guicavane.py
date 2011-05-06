21dc64db8f603bb18e2a23537a743f8e3a79ff0f
j0hn <j0hn.com.ar@gmail.com>
2011-05-06 11:06:29 -0300

    Starting to use threads, moved some code

--- guicavane.py
+++ guicavane.py
@@ -11,6 +11,7 @@ Author: Gonzalo Garcia (A.K.A j0hn) <j0hn.com.ar@gmail.com>
 import os
 import gtk
 import string
+import thread
 
 import pycavane
 from utils import combobox_get_active_text
@@ -35,22 +36,56 @@ class MainWindow:
 
         self.pycavane = pycavane.Pycavane()
 
-        window = self.builder.get_object("mainWindow")
-        window.show_all()
+        # Getting the used widgets
+        self.main_window = self.builder.get_object("mainWindow")
+        self.statusbar = self.builder.get_object("statusbar")
+        self.name_list = self.builder.get_object("nameList")
+        self.name_model = self.name_list.get_model()
+        self.file_viewer = self.builder.get_object("fileViewer")
+        self.file_model = self.file_viewer.get_model()
+        self.seassons_combo = self.builder.get_object("seassonCombo")
+        self.seassons_model = self.seassons_combo.get_model()
+        self.mode_combobox = self.builder.get_object("modeCombo")
+        self.name_filter = self.builder.get_object("nameFilter")
 
         # Creating a new filter model to allow the user filter the
         # shows and movies by typing on an entry box
-        name_list = self.builder.get_object("nameList")
-        name_model = name_list.get_model()
-        self.name_filter = name_model.filter_new()
-        self.name_filter.set_visible_func(self.visible_func)
-        name_list.set_model(self.name_filter)
+        self.name_model_filter = self.name_model.filter_new()
+        self.name_model_filter.set_visible_func(self.visible_func)
+        self.name_list.set_model(self.name_model_filter)
 
         # We leave the magic connection to glade
         self.builder.connect_signals(self)
 
+        # Now we show the window
+        self.main_window.show_all()
+
         # The default mode it's shows
         self.set_mode_shows()
+
+    def thread_safe(f):
+        """
+        Decorator to deny the user to interact with the interface
+        while the program is doing some background task.
+        """
+
+        def decorate(self, *args):
+            self.main_window.set_sensitive(False)
+            res = f(self, *args)
+            self.main_window.set_sensitive(True)
+            return res
+        return decorate
+
+    def background_task(f):
+        """
+        Decorator to start a function in background and not
+        block the interface.
+        """
+
+        def decorate(self, *args):
+            thread.start_new(f, tuple([self] + list(args)))
+
+        return decorate
 
     def on_destroy(self, widget):
         """
@@ -72,6 +107,8 @@ class MainWindow:
         else:
             self.set_mode_movies()
 
+    @background_task
+    @thread_safe
     def on_seasson_change(self, combo):
         """
         Called when the 'seasson' combobox changes value.
@@ -88,9 +125,7 @@ class MainWindow:
         if not seasson:
             return
 
-        file_viewer = self.builder.get_object("fileViewer")
-        file_model = file_viewer.get_model()
-        file_model.clear()
+        self.file_model.clear()
 
         theme = gtk.icon_theme_get_default()
         file_icon = theme.load_icon(gtk.STOCK_FILE, 48, 0)
@@ -98,8 +133,10 @@ class MainWindow:
         seasson = "Temporada " + seasson  # Hopfully temporary fix
         for episode in self.pycavane.episodes_by_season(show, seasson):
             episode_name = "%.2d - %s" % (int(episode[1]), episode[2])
-            file_model.append([file_icon, episode_name])
+            self.file_model.append([file_icon, episode_name])
 
+    @background_task
+    @thread_safe
     def on_name_change(self, treeview):
         """
         Called when the user selects a movie or a show from the 'name list'.
@@ -108,34 +145,47 @@ class MainWindow:
         selected_text = self.get_selected_name()
 
         if self.get_mode() == MODE_SHOWS:
-            seassons_combo = self.builder.get_object("seassonCombo")
-            seassons_model = seassons_combo.get_model()
-            seassons_model.clear()
+            self.seassons_model.clear()
 
             seassons = self.pycavane.seasson_by_show(selected_text)
             for i in range(1, len(seassons)+1):
                 # Here we're assuming that the server has the
                 # seassons 1 to length(seassons) that could not be true. TODO
-                seassons_model.append([i])
+                self.seassons_model.append([i])
+
+        else:
+            self.file_model.clear()
+
+            theme = gtk.icon_theme_get_default()
+            file_icon = theme.load_icon(gtk.STOCK_FILE, 48, 0)
+
+            letter = selected_text
+
+            for movie in self.pycavane.get_movies(letter):
+                self.file_model.append([file_icon, movie[1]])
 
     def on_name_filter_change(self, entry):
-        self.name_filter.refilter()
+        """
+        Called when the textbox to filter names changes.
+        """
 
-    def set_mode_shows(self):
+        self.name_model_filter.refilter()
+
+    @background_task
+    @thread_safe
+    def set_mode_shows(self, *args):
         """
         Sets the current mode to shows.
         """
 
         # We show the seasson combobox
-        seasson_combo = self.builder.get_object("seassonCombo")
-        seasson_combo.set_sensitive(True)
+        self.seassons_combo.set_sensitive(True)
 
-        shows_model = self.builder.get_object("nameListstore")
-        shows_model.clear()
+        self.name_model.clear()
 
         shows = self.pycavane.get_shows()
         for show in shows:
-            shows_model.append([show[1]])
+            self.name_model.append([show[1]])
 
     def set_mode_movies(self):
         """
@@ -143,14 +193,12 @@ class MainWindow:
         """
 
         # We won't be needing the seasson combobox so we hide it
-        seasson_combo = self.builder.get_object("seassonCombo")
-        seasson_combo.set_sensitive(False)
+        self.seassons_combo.set_sensitive(False)
 
-        movies_model = self.builder.get_object("nameListstore")
-        movies_model.clear()
+        self.name_model.clear()
 
         for letter in string.uppercase:
-            movies_model.append([letter])
+            self.name_model.append([letter])
 
     def get_mode(self):
         """
@@ -159,8 +207,7 @@ class MainWindow:
         The result will be the constant MODE_SHOWS or MODE_MOVIES.
         """
 
-        mode_combobox = self.builder.get_object("modeCombo")
-        mode_text = combobox_get_active_text(mode_combobox)
+        mode_text = combobox_get_active_text(self.mode_combobox)
 
         # Poscondition
         assert mode_text == MODE_SHOWS or mode_text == MODE_MOVIES
@@ -172,16 +219,14 @@ class MainWindow:
         Returns the string of the selected item on the 'name list'.
         """
 
-        treeview = self.builder.get_object("nameList")
-        selection = treeview.get_selection()
+        selection = self.name_list.get_selection()
         model, iter = selection.get_selected()
         selected_text = model.get_value(iter, 0)
 
         return selected_text
 
     def visible_func(self, model, iter):
-        filter_entry = self.builder.get_object("nameFilter")
-        filtered_text = filter_entry.get_text()
+        filtered_text = self.name_filter.get_text()
 
         row_text = model.get_value(iter, 0)
 
@@ -194,7 +239,6 @@ class MainWindow:
 
         return False
 
-
 def main():
     """
     Creates the window and starts gtk main loop.
@@ -202,8 +246,8 @@ def main():
 
     guifile = "gui.glade"
     mainWindow = MainWindow(guifile)
+    gtk.gdk.threads_init()
     gtk.main()
-
 
 if __name__ == "__main__":
     main()
