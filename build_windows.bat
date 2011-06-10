31bf6b41cd777e58d2d1171a94cf8a0e296975b1
j0hn <j0hn.com.ar@gmail.com>
2011-06-10 20:35:58 -0300

    Updated windows installer

--- build_windows.bat
+++ build_windows.bat
@@ -4,4 +4,5 @@ cd windows
 xcopy "etc" "build\etc" /D /E /C /R /I /K /Y
 xcopy "lib" "build\lib" /D /E /C /R /I /K /Y
 "%programfiles%\NSIS\makensis.exe" guicavane.nsi
+cd ..
 echo [DONE] Windows build can be found in windows/build and the installer in windows/Guicavane.exe
