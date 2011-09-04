a31f314da03f0f3d404f8f4bea8d6f5186c3de3a
j0hn <j0hn.com.ar@gmail.com>
2011-09-04 20:55:09 -0300

    fixed windows installer for new version

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ outdata_con['dest_base'] = "guicavane_debug"
 
 opts = {
     'py2exe': {
-        'packages': 'encodings, gtk, guicavane',
-        'includes': 'cairo, pangocairo, pango, atk, gobject, os, urllib, urllib2, cookielib, pycavane, guicavane, gio, unicodedata, webbrowser',
+        'packages': 'encodings, gtk, guicavane, guicavane.Downloaders',
+        'includes': 'cairo, pangocairo, pango, atk, gobject, os, urllib, urllib2, cookielib, pycavane, guicavane, guicavane.Downloaders, gio, unicodedata, webbrowser',
         'excludes': ["pywin", "pywin.debugger", "pywin.debugger.dbgcon",
             "pywin.dialogs", "pywin.dialogs.list", "Tkconstants", "Tkinter", "tcl",
             "doctest", "macpath", "pdb", "ftplib", "win32wnet", "getopt",],
@@ -29,14 +29,14 @@ opts = {
     }
 
 files = []
-files.append(("gui", ["guicavane/gui/main.glade", "guicavane/gui/settings.glade"]))
-files.append(("images", ["images/" + x for x in os.listdir("images")]))
-
+files.append(("glade", ["glade\\" + x for x in os.listdir("glade")]))
+files.append(("images", ["images\\" + x for x in os.listdir("images") if not os.path.isdir("images\\" + x)]))
+files.append(("images\\hosts\\", ["images\\hosts\\" + x for x in os.listdir("images\\hosts\\")]))
 
 setup(
     name="Guicavane",
     description="Graphical user interface for www.cuevana.tv",
-    version="0.9",
+    version="1.5.0",
     windows=[outdata_win],
     console=[outdata_con],
     options=opts,
