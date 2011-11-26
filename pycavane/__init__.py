efdb3518232d4367e2c176d21c47fbd75545a32b
j0hn <j0hn.com.ar@gmail.com>
2011-11-26 12:12:18 -0300

    arreglando las peliculas en la api de pycavane

--- pycavane/__init__.py
+++ pycavane/__init__.py
@@ -1,4 +1,4 @@
-#import api
+import api
 #import pelispedia_api as api
 #import moviezet_api as api
-import freevana_api as api
+#import freevana_api as api
--- pycavane/urls.py
+++ pycavane/urls.py
@@ -11,15 +11,17 @@ episodes = host + '/list_search_id.php?temporada=%s'
 login = host + '/login_get.php'
 
 episode = host + '/series/%s/%s/%s/'
-player_movie = host + '/player/sources?id=%s'
-player_season = player_movie + '&tipo=serie'
+sources = host + '/player/sources?id=%s'
+player_movie = sources + '&tipo=pelicula'
+player_season = sources + '&tipo=serie'
 source_get = host + '/player/source_get'
 
 sub_show = static_host + '/files/s/sub/%s_%s.srt'
 sub_show_quality = static_host + '/files/s/sub/%s_%s_%s.srt'
-sub_movie = host + '/download_sub?file=sub/%s_%s.srt'
+sub_movie_quality = static_host + '/files/sub/%s_%s_%s.srt'
+sub_movie = static_host + '/files/sub/%s_%s.srt'
 
-search = host + '/buscar/?q=%s&cat=titulo'
+search = host + '/web/buscar?&q=%s'
 latest_movies = host + '/peliculas/'
 recomended_movies = host
 
