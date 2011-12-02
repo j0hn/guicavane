be5cc50b9c8311a30ac8a1a445e170128362f9bc
j0hn <j0hn.com.ar@gmail.com>
2011-12-02 19:42:20 -0300

    Starting support for Monsterdivx, movies it's still missing..

---
+++ guicavane/Hosts/Monsterdivx/api.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Monsterdivx API.
+API for website www.monsterdivx.com
+
+
+Authors: j0hn <j0hn.com.ar@gmail.com>
+"""
+
+import re
+import json
+
+import urls
+from guicavane.Hosts.Base import *
+from guicavane.Utils.UrlOpen import UrlOpen
+
+DISPLAY_NAME = "Monsterdivx"
+url_open = UrlOpen()
+
+HOST_TRANSLATE = {"mega": "megaupload",
+                  "wupload": "wupload"}
+
+
+class Episode(BaseEpisode):
+    _hosts_re = re.compile("onclick=\"goSource\('(?P<id>.*?)'," \
+                           "'(?P<name>.*?)'\)")
+
+    def __init__(self, id, name, number, season, show, slug):
+        self.id = id
+        self.name = name
+        self.number = number
+        self.season = season
+        self.show = show
+        self.slug = slug
+
+    @property
+    def file_hosts(self):
+        hosts = {}
+
+        data = url_open(urls.sources % self.id)
+        data = data.split('<div id="sources">', 1)[1]
+        data = data.split('<div style="clear:left">', 1)[0]
+
+        for host in self._hosts_re.finditer(data):
+            host = host.groupdict()
+
+            args = {"key": host["id"],
+                    "host": host["name"],
+                    "id": self.id,
+                    "sub": ",ES",
+                    "sub_pre": "ES"}
+
+            hostdata = url_open(urls.source_get, args)
+
+            longname = HOST_TRANSLATE[host["name"]]
+
+            if longname not in hosts:
+                hosts[longname] = {}
+
+            hosts[longname]["360"] = hostdata
+
+        return hosts
+
+    def get_subtitle_url(self, lang="ES", quality=None):
+        return urls.sub_show % (self.id, lang)
+
+
+class Season(BaseSeason):
+
+    def __init__(self, id, name, number, show, slug):
+        self.id = id
+        self.name = name
+        self.number = number
+        self.show = show
+        self.slug = slug
+
+    @property
+    def episodes(self):
+        data = url_open(urls.episodes % self.id)
+        jsondata = json.loads(data)
+
+        for episode in jsondata:
+            id = episode["id"]
+            name = episode["name"]
+            number = int(episode["capitulo"])
+            slug = episode["slug"]
+            yield Episode(id, name, number, self, self.show, slug)
+
+
+class Show(BaseShow):
+    _shows_re = re.compile('<a href="(?P<url>.*?)" rel="(?P<id>\d+)".*?>' \
+                           '(?P<name>.*?)</a>')
+
+    def __init__(self, id, name, url):
+        self.id = id
+        self.name = name
+        self.url = url
+
+    @property
+    def seasons(self):
+        data = url_open(urls.seasons % self.id)
+        jsondata = json.loads(data)
+
+        for season in jsondata:
+            id = season["term_id"]
+            name = season["name"]
+            number = int(name.replace("Temporada", ""))
+            slug = season["slug"]
+            yield Season(id, name, number, self, slug)
+
+    @classmethod
+    def search(self, name=""):
+        name = name.lower()
+
+        data = url_open(urls.shows)
+        data = data.split('<ul id="first-col">', 1)[1]
+        data = data.split('</ul>', 1)[0]
+
+        for show in self._shows_re.finditer(data):
+            show_dict = show.groupdict()
+            if not name or name in show_dict['name'].lower():
+                yield Show(**show_dict)
+
+
+class Movie(BaseMovie):
+
+    def __init__(self, id, name, url):
+        self.id = id
+        self.name = name
+        self.url = url
+
+    @classmethod
+    def search(self, query=""):
+        pass
+
+    def get_subtitle_url(self, lang="ES", quality=None):
+        pass
