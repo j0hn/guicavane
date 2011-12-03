153c8c00bb2e70b4c220b05b91195d3a2433607e
j0hn <j0hn.com.ar@gmail.com>
2011-12-03 02:28:30 -0300

    removed unused stuff in monsterdivx urls

--- guicavane/Hosts/Monsterdivx/urls.py
+++ guicavane/Hosts/Monsterdivx/urls.py
@@ -3,15 +3,11 @@ host = "http://www.monsterdivx.com"
 
 # Shows
 shows = host + "/series/"
-show_info = host + '/web/series?&%s&%s&%s'
 seasons = host + "/wp-content/themes/monsterdivx/scripts/getList.php?id=%s"
 episodes = host + "/wp-content/themes/monsterdivx-old/scripts/getList.php?post&id=%s"
 episode = host + "/%s/"
 
 # Movies
-movie_info = host + '/web/peliculas?&%s&%s'
-latest_movies = host + '/peliculas/'
-recomended_movies = host
 
 # Sources
 sources = host + "/wp-content/plugins/monsterdivx-player/scripts/source-iframe.php?" \
@@ -20,6 +16,3 @@ source_get = host + "/wp-content/plugins/monsterdivx-player/scripts/source_get.p
 
 # Subtitles
 sub_show = host + "/sub/%s_%s.srt"
-
-# Misc
-search = host + '/web/buscar?&q=%s'
